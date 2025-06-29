9d5d3a2dff974ad1b71b379b30b8429b213225d3
shabaz Pathan <shabazshaikh@hotmail.com>
2025-06-29 22:13:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 const CONFIG = {
-  razorpayKey: "rzp_test_rXXsewprnszVhN", // Razorpay TEST key
-  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubk1gb1/", // Zapier webhook
+  razorpayKey: "rzp_test_rXXsewprnszVhN",
+  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubk1gb1/"
 };
