6b57322585ab78c43c340146ce23ca044cd64062
shabaz Pathan <shabazshaikh@hotmail.com>
2025-06-30 18:08:31 +0530

    Update config-public.js

--- config-public.js
+++ config-public.js
@@ -1,5 +1,5 @@
 // config-public.js
 const CONFIG = {
   razorpayKey: "rzp_test_rXXsewprnszVhN", // Test key
-  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubk1gb1/", // Rotate if exposed
+  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubwzz9p/", // Rotate if exposed
 };
