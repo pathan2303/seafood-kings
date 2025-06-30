aec57cac99ec6b39c78402440e49e8b351444aa7
shabaz Pathan <shabazshaikh@hotmail.com>
2025-06-30 17:19:17 +0530

    Update config-public.js

--- config-public.js
+++ config-public.js
@@ -1,4 +1,5 @@
+// config-public.js
 const CONFIG = {
-  razorpayKey: "rzp_test_rXXsewprnszVhN",
-  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubk1gb1/"
+  razorpayKey: "rzp_test_rXXsewprnszVhN", // Replace with LIVE key when going live
+  zapierWebhook: "https://hooks.zapier.com/hooks/catch/23506670/ubk1gb1/",
 };
